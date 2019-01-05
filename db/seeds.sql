f5d48b8c6857e6077b08ed5868f1fca117ff2ac4
K Sharma <sharma.1809k@gmail.com>
2019-01-05 12:26:55 -0500

    done

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Burger' , false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('chicken Burger', false);
+
